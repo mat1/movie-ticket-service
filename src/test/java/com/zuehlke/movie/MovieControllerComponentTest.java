2fcab6e0205bcbb13b037efb1b076227c590ab10
Matthias Brun <matthias.brun@gmail.com>
2017-04-21 09:02:17 +0200

    Fix formatting

--- src/test/java/com/zuehlke/movie/MovieControllerComponentTest.java
+++ src/test/java/com/zuehlke/movie/MovieControllerComponentTest.java
@@ -61,30 +61,30 @@ public class MovieControllerComponentTest {
     @Test
     public void getMovies() throws Exception {
         when().
-                get("/api/v1/movies").
-                then().
-                statusCode(200).
-                body("[0].id", equalTo(1)).
-                body("[0].title", equalTo("Batman Begins")).
-                body("[0].poster", equalTo("https://images-na.ssl-images-amazon.com/images/M/MV5BNTM3OTc0MzM2OV5BMl5BanBnXkFtZTYwNzUwMTI3._V1_SX300.jpg")).
-                body("[1].id", equalTo(2)).
-                body("[2].id", equalTo(3));
+            get("/api/v1/movies").
+        then().
+            statusCode(200).
+            body("[0].id", equalTo(1)).
+            body("[0].title", equalTo("Batman Begins")).
+            body("[0].poster", equalTo("https://images-na.ssl-images-amazon.com/images/M/MV5BNTM3OTc0MzM2OV5BMl5BanBnXkFtZTYwNzUwMTI3._V1_SX300.jpg")).
+            body("[1].id", equalTo(2)).
+            body("[2].id", equalTo(3));
     }
 
     @Test
     public void getMovieById() throws Exception {
         when().
-                get("/api/v1/movies/1").
-                then().
-                statusCode(200).
-                body("id", equalTo(1)).
-                body("title", equalTo("Batman Begins")).
-                body("poster", equalTo("https://images-na.ssl-images-amazon.com/images/M/MV5BNTM3OTc0MzM2OV5BMl5BanBnXkFtZTYwNzUwMTI3._V1_SX300.jpg")).
-                body("plot", equalTo("After training with his mentor, Batman begins his fight to free crime-ridden Gotham City from the corruption that Scarecrow and the League of Shadows have cast upon it.")).
-                body("year", equalTo(2005)).
-                body("genre", equalTo("Action")).
-                body("ratings.source", hasItems("Internet Movie Database", "Rotten Tomatoes")).
-                body("ratings.value", hasItems("8.3/10", "84%"));
+            get("/api/v1/movies/1").
+        then().
+            statusCode(200).
+            body("id", equalTo(1)).
+            body("title", equalTo("Batman Begins")).
+            body("poster", equalTo("https://images-na.ssl-images-amazon.com/images/M/MV5BNTM3OTc0MzM2OV5BMl5BanBnXkFtZTYwNzUwMTI3._V1_SX300.jpg")).
+            body("plot", equalTo("After training with his mentor, Batman begins his fight to free crime-ridden Gotham City from the corruption that Scarecrow and the League of Shadows have cast upon it.")).
+            body("year", equalTo(2005)).
+            body("genre", equalTo("Action")).
+            body("ratings.source", hasItems("Internet Movie Database", "Rotten Tomatoes")).
+            body("ratings.value", hasItems("8.3/10", "84%"));
     }
 
 }
